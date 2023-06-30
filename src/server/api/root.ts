180614dc4ba9c6c12964d39b6b2f97efad3e7e07
jaredgoldman <jaredgoldmandev@gmail.com>
2023-06-30 13:27:28 -0400

    FEAT: Complete venue crud tests

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { eventRouter } from "~/server/api/routers/event"
 import { createTRPCRouter } from "~/server/api/trpc"
+import { venueRouter } from "./routers/venue"
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc"
  */
 export const appRouter = createTRPCRouter({
     event: eventRouter,
+    venue: venueRouter,
 })
 
 // export type definition of API
