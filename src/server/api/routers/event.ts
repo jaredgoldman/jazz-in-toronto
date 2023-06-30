02a44a63a0427bb28a9c534ce2e3a94c8f2cbca1
jaredgoldman <jaredgoldmandev@gmail.com>
2023-06-30 12:36:19 -0400

    FEAT; Finish tests for event router, add cancelled attribute to events, upgrade prisma, refactor to use one eventSchema in router

--- src/server/api/routers/event.ts
+++ src/server/api/routers/event.ts
@@ -1,72 +1,57 @@
-import { z } from "zod";
+import { z } from "zod"
 import {
     createTRPCRouter,
     publicProcedure,
     protectedProcedure,
-} from "~/server/api/trpc";
+} from "~/server/api/trpc"
+
+const eventSchema = z.object({
+    id: z.string().cuid().optional(),
+    name: z.string(),
+    startDate: z.string(),
+    endDate: z.string(),
+    photoPath: z.string().optional(),
+    featured: z.boolean().optional(),
+    instagramHandle: z.string().optional(),
+    website: z.string().optional(),
+    bandId: z.string().cuid(),
+    venueId: z.string().cuid(),
+    cancelled: z.boolean().optional(),
+})
 
 export const eventRouter = createTRPCRouter({
-    create: publicProcedure
-        .input(
-            z.object({
-                name: z.string(),
-                startDate: z.string(),
-                endDate: z.string(),
-                photoPath: z.string().optional(),
-                featured: z.boolean().optional(),
-                instagramHandle: z.string().optional(),
-                website: z.string().optional(),
-                bandId: z.string().cuid(),
-                venueId: z.string().cuid(),
-            })
-        )
-        .query(({ ctx, input }) => {
-            const { bandId, venueId, ...eventData } = input;
-            return ctx.prisma.event.create({
-                data: {
-                    ...eventData,
-                    band: { connect: { id: input.bandId } },
-                    venue: { connect: { id: input.venueId } },
-                },
-            });
-        }),
+    create: publicProcedure.input(eventSchema).query(({ ctx, input }) => {
+        const { bandId, venueId, ...eventData } = input
+        return ctx.prisma.event.create({
+            data: {
+                ...eventData,
+                band: { connect: { id: input.bandId } },
+                venue: { connect: { id: input.venueId } },
+            },
+        })
+    }),
 
     get: publicProcedure
         .input(z.object({ id: z.string().cuid() }))
         .query(({ ctx, input }) => {
             return ctx.prisma.event.findUnique({
                 where: { id: input.id },
-            });
+            })
         }),
 
-    update: protectedProcedure
-        .input(
-            z.object({
-                id: z.string().cuid(),
-                name: z.string().optional(),
-                startDate: z.string().optional(),
-                endDate: z.string().optional(),
-                photoPath: z.string().optional(),
-                featured: z.boolean().optional(),
-                instagramHandle: z.string().optional(),
-                website: z.string().optional(),
-                bandId: z.string().cuid().optional(),
-                venueId: z.string().cuid().optional(),
-            })
-        )
-        .query(({ ctx, input }) => {
-            const { id, ...eventData } = input;
-            return ctx.prisma.event.update({
-                where: { id: input.id },
-                data: eventData,
-            });
-        }),
+    update: protectedProcedure.input(eventSchema).query(({ ctx, input }) => {
+        const { id, ...eventData } = input
+        return ctx.prisma.event.update({
+            where: { id: input.id },
+            data: eventData,
+        })
+    }),
 
     delete: protectedProcedure
         .input(z.object({ id: z.string().cuid() }))
         .query(({ ctx, input }) => {
             return ctx.prisma.event.delete({
                 where: { id: input.id },
-            });
+            })
         }),
-});
+})
--- src/server/api/routers/test/event.test.ts
+++ src/server/api/routers/test/event.test.ts
@@ -1,10 +1,9 @@
-import { expect, it, beforeAll, afterAll } from "vitest"
-
+import { expect, it, describe, beforeAll, afterAll } from "vitest"
 import { type RouterInputs } from "~/utils/api"
 import { appRouter } from "~/server/api/root"
 import { createInnerTRPCContext } from "~/server/api/trpc"
 import { prisma } from "~/server/db"
-import { Band, Event, Venue } from "@prisma/client"
+import { Admin, Band, Event, Venue } from "@prisma/client"
 
 const testVenueData = {
     name: "test venue",
@@ -20,13 +19,15 @@ const testBandData = {
 let venue: Venue
 let band: Band
 let event: Event
+let admin: Admin
 let testEventData: RouterInputs["event"]["create"]
 
 beforeAll(async () => {
-    // create a venue and a band
     venue = await prisma.venue.create({ data: testVenueData })
     band = await prisma.band.create({ data: testBandData })
-
+    admin = await prisma.admin.create({
+        data: { email: "test@test.com", password: "password" },
+    })
     testEventData = {
         name: "test event",
         startDate: new Date(Date.now()).toISOString(),
@@ -36,22 +37,96 @@ beforeAll(async () => {
     }
 })
 
-it("unauthed user should be able to create an event", async () => {
-    const ctx = createInnerTRPCContext({ session: null })
-    const caller = appRouter.createCaller(ctx)
+describe("Event Router", () => {
+    it("should allow an unauthorized user to create an event", async () => {
+        const ctx = createInnerTRPCContext({ session: null })
+        const caller = appRouter.createCaller(ctx)
 
-    const input: RouterInputs["event"]["create"] = testEventData
+        const input: RouterInputs["event"]["create"] = testEventData
 
-    event = await caller.event.create(input)
+        event = await caller.event.create(input)
 
-    const query = await prisma.event.findMany({
-        where: { name: testEventData.name },
+        const query = await prisma.event.findMany({
+            where: { name: testEventData.name },
+        })
+        expect(query.length).toEqual(1)
     })
-    expect(query.length).toEqual(1)
+
+    it("should allow an authorized user to edit an event", async () => {
+        const ctx = createInnerTRPCContext({
+            session: {
+                user: {
+                    id: admin.id,
+                    name: admin.name,
+                    email: admin.email,
+                },
+                expires: "1",
+            },
+        })
+        const caller = appRouter.createCaller(ctx)
+
+        const input: RouterInputs["event"]["update"] = {
+            id: event.id,
+            name: "updated event name",
+        }
+
+        const updatedEvent = await caller.event.update(input)
+
+        expect(updatedEvent.name).toEqual(input.name)
+
+        // Revert event name
+        const revertInput: RouterInputs["event"]["update"] = {
+            id: event.id,
+            name: event.name,
+        }
+
+        const revertedEvent = await caller.event.update(revertInput)
+
+        expect(revertedEvent.name).toEqual(event.name)
+    })
+    it("should allow an authorized user to fetch an event", async () => {
+        const ctx = createInnerTRPCContext({
+            session: {
+                user: {
+                    id: admin.id,
+                    name: admin.name,
+                    email: admin.email,
+                },
+                expires: "1",
+            },
+        })
+        const caller = appRouter.createCaller(ctx)
+
+        const input: RouterInputs["event"]["get"] = { id: event.id }
+
+        const fetchedEvent = await caller.event.get(input)
+
+        expect(fetchedEvent?.name).toEqual(event.name)
+    })
+
+    it("should allow an authorized user to delete an event", async () => {
+        const ctx = createInnerTRPCContext({
+            session: {
+                user: {
+                    id: admin.id,
+                    name: admin.name,
+                    email: admin.email,
+                },
+                expires: "1",
+            },
+        })
+        const caller = appRouter.createCaller(ctx)
+
+        const input: RouterInputs["event"]["delete"] = { id: event.id }
+
+        const deletedEvent = await caller.event.delete(input)
+
+        expect(deletedEvent.id).toEqual(event.id)
+    })
 })
 
 afterAll(async () => {
-    await prisma.event.delete({ where: { id: event.id } })
     await prisma.venue.delete({ where: { id: venue.id } })
     await prisma.band.delete({ where: { id: band.id } })
+    await prisma.admin.delete({ where: { id: admin.id } })
 })
