040109e5ba9554beee21b5a355c1afb9e25a3739
jaredgoldman <jaredgoldmandev@gmail.com>
2023-07-01 11:29:21 -0400

    FEAT: Add band router

---
+++ prisma/seed.ts
@@ -0,0 +1,18 @@
+export const userData = {
+    admin1: {
+        email: "testadmin1@test.com",
+        password: "password",
+    },
+    admin2: {
+        email: "testadmin2@test.com",
+        password: "password",
+    },
+    superAdmin1: {
+        email: "superadmin@test.com",
+        password: "password",
+    },
+    superAdmin2: {
+        email: "superAdmin2@test.com",
+        password: "password",
+    },
+}
---
+++ src/server/api/routers/band.ts
@@ -0,0 +1,61 @@
+import { z } from "zod"
+import {
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
+} from "~/server/api/trpc"
+
+export const bandRouter = createTRPCRouter({
+    create: publicProcedure
+        .input(
+            z.object({
+                name: z.string(),
+                genre: z.string().optional(),
+                photoPath: z.string().optional(),
+                featured: z.boolean().optional(),
+                instagramHandle: z.string().optional(),
+                website: z.string().optional(),
+            })
+        )
+        .query(({ ctx, input }) => {
+            return ctx.prisma.band.create({
+                data: input,
+            })
+        }),
+
+    get: publicProcedure
+        .input(z.object({ id: z.string().cuid() }))
+        .query(({ ctx, input }) => {
+            return ctx.prisma.band.findUnique({
+                where: { id: input.id },
+            })
+        }),
+
+    update: protectedProcedure
+        .input(
+            z.object({
+                id: z.string().cuid(),
+                name: z.string().optional(),
+                genre: z.string().optional(),
+                photoPath: z.string().optional(),
+                featured: z.boolean().optional(),
+                instagramHandle: z.string().optional(),
+                website: z.string().optional(),
+            })
+        )
+        .query(({ ctx, input }) => {
+            const { id, ...bandData } = input
+            return ctx.prisma.band.update({
+                where: { id: input.id },
+                data: bandData,
+            })
+        }),
+
+    delete: protectedProcedure
+        .input(z.object({ id: z.string().cuid() }))
+        .query(({ ctx, input }) => {
+            return ctx.prisma.band.delete({
+                where: { id: input.id },
+            })
+        }),
+})
