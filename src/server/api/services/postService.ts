0214a6bafa409d2b71325def981b0b7dfd4b82c3
jaredgoldman <jaredgoldmandev@gmail.com>
2023-07-31 18:22:33 -0400

    FIX: comment out post service for now

--- src/server/api/services/postService.ts
+++ src/server/api/services/postService.ts
@@ -1,78 +1,76 @@
-// Types
-import { type EventWithBandVenue } from '~/types/data'
-// Services
-import type CanvasService from './canvasService'
-import { type CloudinaryService } from '~/types/library'
-
-export default class postService {
-    private events: EventWithBandVenue[]
-    private cloudinary: CloudinaryService
-
-    constructor(events: EventWithBandVenue[], cloudinary: CloudinaryService) {
-        this.events = events
-        this.cloudinary = cloudinary
-
-        cloudinary.config({
-            secure: true
-        })
-    }
-
-    public async createSavePost(
-        canvasService: CanvasService,
-        date: Date
-    ): Promise<string[]> {
-        const fileUrls = this.createPost(canvasService, date)
-        if (fileUrls) {
-            const storedUrls = await this.storePosts(fileUrls)
-            // await this.postToInstagram(storedUrls)
-            return storedUrls
-            // await this.deleteStored()
-        } else {
-            throw new Error('Error creating post')
-        }
-    }
-
-    private createPost(
-        canvasService: CanvasService,
-        date: Date
-    ): string[] | void {
-        const fileUrls = canvasService.createPosts(this.events, date)
-        if (fileUrls) {
-            return fileUrls
-        } else {
-            throw new Error("Couldn't create posts")
-        }
-    }
-
-    private async storePosts(fileUrls: string[]): Promise<Array<string>> {
-        const storedUrls: Array<string> = []
-        for (const fileUrl of fileUrls) {
-            const options = {
-                use_filename: true,
-                unique_filename: false,
-                overwrite: true
-            }
-
-            try {
-                const result = await this.cloudinary.uploader.upload(
-                    fileUrl,
-                    options
-                )
-                storedUrls.push(result.secure_url)
-            } catch (error) {
-                throw new Error("Couldn't store posts")
-            }
-        }
-        return storedUrls
-    }
-
-    // private async postToInstagram(storedUrls: string[]): Promise<void> {
-    //     storedUrls.forEach((storedUrl) => {
-    //         try {
-    //             // post each image to insta
-    //         } catch (e) {}
-    //     })
-    // }
-
-    // private async deleteStored(): Promise<void> {}
-}
+// // Types
+// import { type EventWithBandVenue } from '~/types/data'
+// // Services
+// import { type CloudinaryService } from '~/types/library'
+//
+// export default class postService {
+//     private events: EventWithBandVenue[]
+//     private cloudinary: CloudinaryService
+//
+//     constructor(events: EventWithBandVenue[], cloudinary: CloudinaryService) {
+//         this.events = events
+//         this.cloudinary = cloudinary
+//
+//         cloudinary.config({
+//             secure: true
+//         })
+//     }
+//
+//     public async createSavePost(
+//         date: Date
+//     ): Promise<string[]> {
+//         const fileUrls = this.createPost(canvasService, date)
+//         if (fileUrls) {
+//             const storedUrls = await this.storePosts(fileUrls)
+//             // await this.postToInstagram(storedUrls)
+//             return storedUrls
+//             // await this.deleteStored()
+//         } else {
+//             throw new Error('Error creating post')
+//         }
+//     }
+//
+//     private createPost(
+//         canvasService: CanvasService,
+//         date: Date
+//     ): string[] | void {
+//         const fileUrls = canvasService.createPosts(this.events, date)
+//         if (fileUrls) {
+//             return fileUrls
+//         } else {
+//             throw new Error("Couldn't create posts")
+//         }
+//     }
+//
+//     private async storePosts(fileUrls: string[]): Promise<Array<string>> {
+//         const storedUrls: Array<string> = []
+//         for (const fileUrl of fileUrls) {
+//             const options = {
+//                 use_filename: true,
+//                 unique_filename: false,
+//                 overwrite: true
+//             }
+//
+//             try {
+//                 const result = await this.cloudinary.uploader.upload(
+//                     fileUrl,
+//                     options
+//                 )
+//                 storedUrls.push(result.secure_url)
+//             } catch (error) {
+//                 throw new Error("Couldn't store posts")
+//             }
+//         }
+//         return storedUrls
+//     }
+//
+//     // private async postToInstagram(storedUrls: string[]): Promise<void> {
+//     //     storedUrls.forEach((storedUrl) => {
+//     //         try {
+//     //             // post each image to insta
+//     //         } catch (e) {}
+//     //     })
+//     // }
+//
+//     // private async deleteStored(): Promise<void> {}
+// }
