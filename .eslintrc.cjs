2a5f0c42e6005a7572f9c9d36e34f59252eeaaa3
jaredgoldman <jaredgoldmandev@gmail.com>
2023-08-01 09:18:42 -0400

    FIX: Turn off no-unescaped entities lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ const config = {
     plugins: ['@typescript-eslint'],
     extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
     rules: {
+        'react/no-unescaped-entities': 'off',
         '@typescript-eslint/no-floating-promises': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/consistent-type-imports': [
