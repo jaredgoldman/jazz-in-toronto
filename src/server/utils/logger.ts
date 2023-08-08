4cb475ffa011247514564387be6cdd0cc0b6ea81
jaredgoldman <jaredgoldmandev@gmail.com>
2023-08-08 09:31:32 -0400

    FIX: Remove pino-pretty and pino-http

--- src/server/utils/logger.ts
+++ src/server/utils/logger.ts
@@ -1,54 +1,8 @@
+// logger.js
 import pino from 'pino'
-import pinoHttp from 'pino-http'
 
-const pinoLogger = pino(
-    {
-        formatters: {
-            level: (label) => {
-                return { level: label }
-            }
-        }
-    },
-    pino.transport({
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            ignore: 'pid,hostname',
-            translateTime: 'yyyy-mm-dd HH:MM:ss.l'
-        }
-    }) as pino.DestinationStream
-)
-
-const httpLogger = pinoHttp({
-    logger: pinoLogger,
-    customLogLevel: (_, res, err) => {
-        if (res.statusCode >= 500 || err) {
-            return 'error'
-        }
-        if (res.statusCode >= 400) {
-            return 'warn'
-        }
-        return 'info'
-    }
+const logger = pino({
+    level: process.env.LOG_LEVEL || 'info'
 })
 
-export class Logger {
-    public static info(message: string, data?: unknown) {
-        pinoLogger.info(message, data)
-    }
-    public static warn(message: string, data?: unknown) {
-        pinoLogger.warn(message, data)
-    }
-    public static error(message: string, data?: unknown) {
-        pinoLogger.error(message, data)
-    }
-    public static debug(message: string, data?: unknown) {
-        pinoLogger.debug(message, data)
-    }
-}
-
-export const initLog = (): string => {
-    return 'Database connection has been established successfully.'
-}
-
-export default httpLogger
+export default logger
