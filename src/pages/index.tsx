52fb43f2bb26358aa7e5fc2c136b0214066edea7
jaredgoldman <jaredgoldmandev@gmail.com>
2023-08-09 00:37:06 -0400

    FEAT: Hydrate TRPC state with getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,29 @@
 // Components
 import RootLayout from '~/layouts/RootLayout'
 import Featured from '~/components/Featured'
+import { createServerSideHelpers } from '@trpc/react-query/server'
+import { appRouter } from '~/server/api/root'
+import superjson from 'superjson'
+import { prisma } from '~/server/db'
+
+export async function getStaticProps() {
+    const helpers = createServerSideHelpers({
+        router: appRouter,
+        ctx: {
+            prisma,
+            session: null
+        },
+        transformer: superjson
+    })
+
+    await helpers.featured.getFeatured.prefetch()
+    return {
+        props: {
+            trpcState: helpers.dehydrate()
+        },
+        revalidate: 1
+    }
+}
 
 export default function Home() {
     return (
