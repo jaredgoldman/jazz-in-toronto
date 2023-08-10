352cbf65dfd72ff5847b2fa8496f59cbbaf9e7b3
jaredgoldman <jaredgoldmandev@gmail.com>
2023-08-09 22:03:31 -0400

    FIX: Adjust width of button container in events admin page"

--- src/pages/admin/events.tsx
+++ src/pages/admin/events.tsx
@@ -37,7 +37,7 @@ export default function AdminEvents(): JSX.Element {
     return (
         <AdminLayout>
             <>
-                <div className="flex w-full justify-center">
+                <div className="flex w-1/5 justify-between">
                     <Button
                         onClick={() => setView(View.Search)}
                         disabled={view === View.Search}
