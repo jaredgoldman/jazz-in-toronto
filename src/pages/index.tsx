a2a3f7186d0306099a45397c12ca82207fabb7a5
jaredgoldman <jaredgoldmandev@gmail.com>
2023-08-16 21:54:57 -0400

    FEAT: Add metrics to admin dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export async function getStaticProps() {
         transformer: superjson
     })
 
-    await helpers.featured.getFeatured.prefetch()
+    await helpers.data.getFeatured.prefetch()
     return {
         props: {
             trpcState: helpers.dehydrate()
