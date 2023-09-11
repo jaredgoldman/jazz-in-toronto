c5ffdc3abc296d50ec915196b25745fb1c934727
jaredgoldman <jaredgoldmandev@gmail.com>
2023-09-11 10:15:55 -0400

    FIX: Update cron job to not use trpc api handler

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,6 +1,19 @@
-import { api } from '~/utils/api'
+import { prisma } from '~/server/db'
+import EmailService from '~/server/api/services/emailService'
+import { env } from '~/env.mjs'
 
-export default function handler() {
-    const { mutate } = api.event.emailUnapproved.useMutation()
-    return mutate()
+export default async function handler() {
+    const emailService = new EmailService()
+    const admins = await prisma.admin.findMany()
+    const unapproved = await prisma.event.count({
+        where: { approved: false, startDate: { gte: new Date() } }
+    })
+    for (const admin of admins) {
+        await emailService.sendEmail(
+            env.EMAIL_FROM,
+            admin.email,
+            'ACTION: Unapproved Events',
+            `There are ${unapproved} unapproved events. Please visit ${env.BASE_URL}/admin/events to approve them.`
+        )
+    }
 }
