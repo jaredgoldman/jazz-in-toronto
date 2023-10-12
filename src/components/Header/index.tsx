bca6b2a0f1df94e1c03d8625d2124c9cc3c8e452
clareysean <seanpclarey@gmail.com>
2023-10-11 22:43:26 -0400

    center nav, clean up underline class and use gap on navlinks for spacing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -32,114 +32,125 @@ export default function Header({
     const { data: session } = useSession()
     return (
         <header>
-            <Flex width="100%" align="end" px="4" py="5">
-                <Flex align="center">
-                    <Heading ml="1" mr="6" size="7">
-                        <NextLink
-                            href="/"
-                            className="text-black hover:text-orange-400 dark:text-white dark:hover:text-orange-400"
-                        >
-                            JAZZINTORONTO
-                        </NextLink>
-                    </Heading>
+            <div className="mx-auto inline-block flex w-[63%] justify-between">
+                <Flex
+                    width="100%"
+                    align="end"
+                    justify="between"
+                    px="4"
+                    mx="auto"
+                    py="5"
+                >
+                    <Flex align="center">
+                        <Heading ml="1" mr="6" size="7">
+                            <NextLink
+                                href="/"
+                                className="text-black hover:text-orange-400 dark:text-white dark:hover:text-orange-400 sm:text-[4.5vmin]"
+                            >
+                                JAZZINTORONTO
+                            </NextLink>
+                        </Heading>
+                    </Flex>
+                    <Flex justify="end" mx="4" width="100%">
+                        {showLinks && (
+                            <nav
+                                className={`flex ${hepta_slab.className} font-medium`}
+                            >
+                                <ul className="flex gap-4">
+                                    {headerType === HeaderType.Public && (
+                                        <>
+                                            <li className={styles.underline}>
+                                                <Link
+                                                    href="/book"
+                                                    size="3"
+                                                    className={
+                                                        styles.noUnderline
+                                                    }
+                                                >
+                                                    Add Your Gig
+                                                </Link>
+                                            </li>
+                                            <li className={styles.underline}>
+                                                <Link
+                                                    href="/listings"
+                                                    size="3"
+                                                    className={
+                                                        styles.noUnderline
+                                                    }
+                                                >
+                                                    Listings
+                                                </Link>
+                                            </li>
+                                            <li className={styles.underline}>
+                                                <Link
+                                                    href="/venues"
+                                                    size="3"
+                                                    className={
+                                                        styles.noUnderline
+                                                    }
+                                                >
+                                                    Venues
+                                                </Link>
+                                            </li>
+                                            <li className={styles.underline}>
+                                                <Link
+                                                    href="/about"
+                                                    size="3"
+                                                    className={
+                                                        styles.noUnderline
+                                                    }
+                                                >
+                                                    About Us
+                                                </Link>
+                                            </li>
+                                        </>
+                                    )}
+                                    {headerType === HeaderType.Admin && (
+                                        <>
+                                            <li className="px-2">
+                                                <Link href="/admin" size="5">
+                                                    Dashboard
+                                                </Link>
+                                            </li>
+                                            <li className="px-2">
+                                                <Link
+                                                    href="/admin/events"
+                                                    size="5"
+                                                >
+                                                    Events
+                                                </Link>
+                                            </li>
+                                            <li className="px-2">
+                                                <Link
+                                                    href="/admin/artists"
+                                                    size="5"
+                                                >
+                                                    Artists
+                                                </Link>
+                                            </li>
+                                            <li className="px-2">
+                                                <Link
+                                                    href="/admin/venues"
+                                                    size="5"
+                                                >
+                                                    Venues
+                                                </Link>
+                                            </li>
+                                        </>
+                                    )}
+                                </ul>
+                            </nav>
+                        )}
+                    </Flex>
                 </Flex>
-                <Flex justify="between" width="100%">
-                    {showLinks && (
-                        <nav
-                            className={`flex ${hepta_slab.className} font-medium`}
-                        >
-                            <ul className="flex">
-                                {headerType === HeaderType.Public && (
-                                    <>
-                                        <li
-                                            // tried changing the config at .eslintrc.cjs but it didn't work
-                                            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                                            className={`${styles.underline} px-2`}
-                                        >
-                                            <Link
-                                                href="/book"
-                                                size="3"
-                                                className={styles.noUnderline}
-                                            >
-                                                Add Your Gig
-                                            </Link>
-                                        </li>
-                                        <li
-                                            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                                            className={`${styles.underline} px-2`}
-                                        >
-                                            <Link
-                                                href="/listings"
-                                                size="3"
-                                                className={styles.noUnderline}
-                                            >
-                                                Listings
-                                            </Link>
-                                        </li>
-                                        <li
-                                            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                                            className={`${styles.underline} px-2`}
-                                        >
-                                            <Link
-                                                href="/venues"
-                                                size="3"
-                                                className={styles.noUnderline}
-                                            >
-                                                Venues
-                                            </Link>
-                                        </li>
-                                        <li
-                                            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                                            className={`${styles.underline} px-2`}
-                                        >
-                                            <Link
-                                                href="/about"
-                                                size="3"
-                                                className={styles.noUnderline}
-                                            >
-                                                About Us
-                                            </Link>
-                                        </li>
-                                    </>
-                                )}
-                                {headerType === HeaderType.Admin && (
-                                    <>
-                                        <li className="px-2">
-                                            <Link href="/admin" size="5">
-                                                Dashboard
-                                            </Link>
-                                        </li>
-                                        <li className="px-2">
-                                            <Link href="/admin/events" size="5">
-                                                Events
-                                            </Link>
-                                        </li>
-                                        <li className="px-2">
-                                            <Link
-                                                href="/admin/artists"
-                                                size="5"
-                                            >
-                                                Artists
-                                            </Link>
-                                        </li>
-                                        <li className="px-2">
-                                            <Link href="/admin/venues" size="5">
-                                                Venues
-                                            </Link>
-                                        </li>
-                                    </>
-                                )}
-                            </ul>
-                        </nav>
-                    )}
-                </Flex>
-                <Flex justify="end" gap="4">
+                <Flex justify="end" align="center" gap="4">
                     {headerType === HeaderType.Admin && session && (
                         <Button onClick={() => void signOut()}>Sign Out</Button>
                     )}
-                    <DarkModeToggle />
+                    <DarkModeToggle className="mb-[-10px]" />
                 </Flex>
-            </Flex>
+            </div>
+
             <Separator size="4" />
         </header>
     )
