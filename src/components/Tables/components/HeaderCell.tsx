60293b4d2866068debe8137797be518a14006b11
jaredgoldman <jaredgoldmandev@gmail.com>
2023-12-17 14:38:20 -0500

    feat: add sorting and empty placeholder states to all tables

---
+++ src/components/Tables/components/HeaderCell.tsx
@@ -0,0 +1,36 @@
+import { Table, Flex, Box } from '@radix-ui/themes'
+import { Header } from '@tanstack/react-table'
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+import { flexRender } from '@tanstack/react-table'
+
+export type Props<TData> = {
+    header: Header<TData, unknown>
+}
+
+export function HeaderCell<TData>({ header }: Props<TData>) {
+    const sortingIcons = {
+        asc: <ChevronUpIcon />,
+        desc: <ChevronDownIcon />
+    }
+
+    return (
+        <Table.ColumnHeaderCell
+            onClick={() => header.column.toggleSorting()}
+            className={header.column.getCanSort() ? 'cursor-pointer select-none' : ''}
+        >
+            <Flex align="center" gap="2">
+                {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                      )}
+                <Box>
+                    {sortingIcons[
+                        header.column.getIsSorted() as 'desc' | 'asc'
+                    ] ?? null}
+                </Box>
+            </Flex>
+        </Table.ColumnHeaderCell>
+    )
+}
