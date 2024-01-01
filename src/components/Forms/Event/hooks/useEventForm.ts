d19b782933f1dac76de9a738af7bcdc52b8bd32d
velveteen-rever <velveteen.rever@gmail.com>
2023-12-31 22:22:06 -0700

    Events almost done, date for selected even doesn't show in form. todo:confirmation alerts.

--- src/components/Forms/Event/hooks/useEventForm.ts
+++ src/components/Forms/Event/hooks/useEventForm.ts
@@ -1,136 +1,136 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { api } from "~/utils/api";
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { api } from '~/utils/api'
 import {
-	type EventWithArtistVenue,
-	type Artist,
-	type Venue,
-} from "~/types/data";
-import { isArtist, isVenue } from "~/utils/typeguards";
+    type EventWithArtistVenue,
+    type Artist,
+    type Venue
+} from '~/types/data'
+import { isArtist, isVenue } from '~/utils/typeguards'
 
 export interface EventFormValues {
-	name: string;
-	startDate: Date;
-	endDate: Date;
-	artistId: string;
-	instagramHandle?: string;
-	website?: string;
-	venueId: string;
-	featured: boolean;
+    name: string
+    startDate: Date
+    endDate: Date
+    artistId: string
+    instagramHandle?: string
+    website?: string
+    venueId: string
+    featured: boolean
 }
 
 export default function useEventForm(currentValues?: EventWithArtistVenue) {
-	const [error, setError] = useState<string>("");
+    const [error, setError] = useState<string>('')
 
-	const {
-		data: venueData,
-		refetch: refetchVenues,
-		isLoading: venuesLoading,
-	} = api.venue.getAll.useQuery();
+    const {
+        data: venueData,
+        refetch: refetchVenues,
+        isLoading: venuesLoading
+    } = api.venue.getAll.useQuery()
 
-	const {
-		data: artistData,
-		refetch: refetchArtists,
-		isLoading: artistsLoading,
-	} = api.artist.getAll.useQuery();
+    const {
+        data: artistData,
+        refetch: refetchArtists,
+        isLoading: artistsLoading
+    } = api.artist.getAll.useQuery()
 
-	const { mutateAsync: eventMutation, isSuccess: eventMutationIsSuccess } =
-		api.event.create.useMutation();
-	const {
-		mutateAsync: editEventMutation,
-		isSuccess: editEventMutationIsSuccess,
-	} = api.event.update.useMutation();
+    const { mutateAsync: eventMutation, isSuccess: eventMutationIsSuccess } =
+        api.event.create.useMutation()
+    const {
+        mutateAsync: editEventMutation,
+        isSuccess: editEventMutationIsSuccess
+    } = api.event.update.useMutation()
 
-	const isLoading = venuesLoading || artistsLoading;
-	const isEditing = !!currentValues;
-	const defaultValues: EventFormValues = currentValues
-		? {
-				...currentValues,
-				instagramHandle: currentValues.instagramHandle || undefined,
-				website: currentValues.website || undefined,
-				featured: currentValues.featured || false,
-		  }
-		: {
-				name: "",
-				startDate: new Date(),
-				endDate: new Date(),
-				artistId: "",
-				venueId: "",
-				instagramHandle: "",
-				website: "",
-				featured: false,
-		  };
+    const isLoading = venuesLoading || artistsLoading
+    const isEditing = !!currentValues
+    const defaultValues: EventFormValues = currentValues
+        ? {
+              ...currentValues,
+              instagramHandle: currentValues.instagramHandle || undefined,
+              website: currentValues.website || undefined,
+              featured: currentValues.featured || false
+          }
+        : {
+              name: '',
+              startDate: new Date(),
+              endDate: new Date(),
+              artistId: '',
+              venueId: '',
+              instagramHandle: '',
+              website: '',
+              featured: false
+          }
 
-	const {
-		register,
-		handleSubmit,
-		setValue,
-		control,
-		getValues,
-		reset,
-		formState: { errors },
-	} = useForm<EventFormValues>({ defaultValues });
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        control,
+        getValues,
+        reset,
+        formState: { errors }
+    } = useForm<EventFormValues>({ defaultValues })
 
-	const onSubmit = async (values: EventFormValues) => {
-		try {
-			if (isEditing && currentValues && editEventMutation) {
-				await editEventMutation({
-					id: currentValues?.id,
-					...values,
-				});
-			} else if (eventMutation) {
-				await eventMutation(values);
-			}
-		} catch (e) {
-			setError("There was an error submitting. Please try again");
-		}
-	};
+    const onSubmit = async (values: EventFormValues) => {
+        try {
+            if (isEditing && currentValues && editEventMutation) {
+                await editEventMutation({
+                    id: currentValues?.id,
+                    ...values
+                })
+            } else if (eventMutation) {
+                await eventMutation(values)
+            }
+        } catch (e) {
+            setError('There was an error submitting. Please try again')
+        }
+    }
 
-	// TODO: Factor into single function
-	const onAddArtist = async (value: Artist | Venue) => {
-		if (isArtist(value)) {
-			await refetchArtists();
-			setValue("artistId", value.id);
-		}
-	};
-	const onAddVenue = async (value: Artist | Venue) => {
-		if (isVenue(value)) {
-			await refetchVenues();
-			setValue("venueId", value.id);
-		}
-	};
+    // TODO: Factor into single function
+    const onAddArtist = async (value: Artist | Venue) => {
+        if (isArtist(value)) {
+            await refetchArtists()
+            setValue('artistId', value.id)
+        }
+    }
+    const onAddVenue = async (value: Artist | Venue) => {
+        if (isVenue(value)) {
+            await refetchVenues()
+            setValue('venueId', value.id)
+        }
+    }
 
-	// Grab the specific artist data from the artist data array
-	// Helpful when editing an event in state in the eventScraper
-	const getSpecificArtistData = () => {
-		if (artistData) {
-			return artistData.filter(
-				(artist) => artist.id === getValues().artistId,
-			)[0];
-		}
-	};
+    // Grab the specific artist data from the artist data array
+    // Helpful when editing an event in state in the eventScraper
+    const getSpecificArtistData = () => {
+        if (artistData) {
+            return artistData.filter(
+                (artist) => artist.id === getValues().artistId
+            )[0]
+        }
+    }
 
-	const submit = handleSubmit(async (data) => {
-		await onSubmit(data);
-	});
+    const submit = handleSubmit(async (data) => {
+        await onSubmit(data)
+    })
 
-	return {
-		isEditing,
-		venueData,
-		artistData,
-		eventMutationIsSuccess,
-		editEventMutationIsSuccess,
-		submit,
-		onAddArtist,
-		onAddVenue,
-		isLoading,
-		register,
-		error,
-		errors,
-		setValue,
-		getValues,
-		control,
-		getSpecificArtistData,
-		reset,
-	};
+    return {
+        isEditing,
+        venueData,
+        artistData,
+        eventMutationIsSuccess,
+        editEventMutationIsSuccess,
+        submit,
+        onAddArtist,
+        onAddVenue,
+        isLoading,
+        register,
+        error,
+        errors,
+        setValue,
+        getValues,
+        control,
+        getSpecificArtistData,
+        reset
+    }
 }
--- src/components/Tables/components/TableActionMenu.tsx
+++ src/components/Tables/components/TableActionMenu.tsx
@@ -1,44 +1,84 @@
-import { DropdownMenu } from "@radix-ui/themes";
-import { DotsVerticalIcon } from "@radix-ui/react-icons";
+import { DropdownMenu, AlertDialog, Button, Flex } from '@radix-ui/themes'
+import { DotsVerticalIcon } from '@radix-ui/react-icons'
+import { useState } from 'react'
 
 type Props = {
-	isFeatured: boolean;
-	onToggleFeatured: () => void;
-	onEdit: () => void;
-	onDelete: () => void;
-};
+    isFeatured: boolean
+    onToggleFeatured: () => void
+    onEdit: () => void
+    onDelete: () => void
+}
 
 export function TableActionMenu({
-	isFeatured,
-	onToggleFeatured,
-	onEdit,
-	onDelete,
+    isFeatured,
+    onToggleFeatured,
+    onEdit,
+    onDelete
 }: Props) {
-	return (
-		<>
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger>
-					<DotsVerticalIcon />
-				</DropdownMenu.Trigger>
-				<DropdownMenu.Content size="2">
-					<DropdownMenu.Item onClick={onEdit}>Edit</DropdownMenu.Item>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item
-						color={isFeatured ? undefined : "green"}
-						onClick={() => {
-							onToggleFeatured();
-						}}
-					>
-						{isFeatured ? "Unfeature" : "Feature"}
-					</DropdownMenu.Item>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item>Approve</DropdownMenu.Item>
-					<DropdownMenu.Separator />
-					<DropdownMenu.Item color="red" onClick={onDelete}>
-						Delete
-					</DropdownMenu.Item>
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
-		</>
-	);
+    const [open, setOpen] = useState(false)
+
+    return (
+        <>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                    <DotsVerticalIcon />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content size="2">
+                    <DropdownMenu.Item onClick={onEdit}>Edit</DropdownMenu.Item>
+                    <DropdownMenu.Separator />
+                    <DropdownMenu.Item
+                        color={isFeatured ? undefined : 'green'}
+                        onClick={() => {
+                            onToggleFeatured()
+                        }}
+                    >
+                        {isFeatured ? 'Unfeature' : 'Feature'}
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Separator />
+                    <DropdownMenu.Item>Approve</DropdownMenu.Item>
+                    <DropdownMenu.Separator />
+                    <AlertDialog.Root open={open} onOpenChange={setOpen}>
+                        <AlertDialog.Trigger>
+                            <DropdownMenu.Item
+                                color="red"
+                                onClick={() => {
+                                    setOpen(true)
+                                }}
+                            >
+                                Delete
+                            </DropdownMenu.Item>
+                        </AlertDialog.Trigger>
+                        <AlertDialog.Content style={{ maxWidth: 450 }}>
+                            <AlertDialog.Title>
+                                Confirm Delete
+                            </AlertDialog.Title>
+                            <AlertDialog.Description size="2">
+                                You are about to delete a record. This action
+                                cannot be undone.
+                            </AlertDialog.Description>
+                            <Flex gap="3" mt="4" justify="end">
+                                <AlertDialog.Cancel>
+                                    <Button variant="soft" color="gray">
+                                        Cancel
+                                    </Button>
+                                </AlertDialog.Cancel>
+                                <AlertDialog.Action>
+                                    <Button
+                                        variant="solid"
+                                        color="red"
+                                        onClick={() => {
+                                            onDelete()
+                                            setOpen(false)
+                                        }}
+                                    >
+                                        Delete
+                                    </Button>
+                                </AlertDialog.Action>
+                            </Flex>
+                        </AlertDialog.Content>
+                    </AlertDialog.Root>
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
+        </>
+    )
 }
