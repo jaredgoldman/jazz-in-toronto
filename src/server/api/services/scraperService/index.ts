b8069809fae9db59f95ecf908fa7f4093ef9ea3b
jaredgoldman <jaredgoldmandev@gmail.com>
2024-01-21 18:13:49 -0500

    fix: remove unnecessary comments

--- src/server/api/services/scraperService/index.ts
+++ src/server/api/services/scraperService/index.ts
@@ -1,16 +1,12 @@
-// Libraries
 import { cheerioJsonMapper, JsonTemplate } from 'cheerio-json-mapper'
 import { TRPCError } from '@trpc/server'
 import puppeterr, { Page } from 'puppeteer-core'
 import { createId } from '@paralleldrive/cuid2'
-// types
 import type { Venue, EventWithArtistVenue } from '~/types/data'
-// Utils
 import chromium from '@sparticuz/chromium-min'
 import { wait } from '~/utils/shared'
 import type { RexEvent, VenueEvents } from './types'
 import { env } from '~/env.mjs'
-// Data
 import rexJson from './templates/rex.json'
 
 export default class ScraperService {
