fac15081de5de634845eaead24a15633195978cd
velveteen-rever <velveteen.rever@gmail.com>
2024-01-21 19:38:39 -0700

    fix: syntax errors

--- src/components/Forms/Fields/PlacesAutoComplete.tsx
+++ src/components/Forms/Fields/PlacesAutoComplete.tsx
@@ -64,7 +64,7 @@ export default function PlacesAutocomplete<T extends FieldValues>({
     
     useEffect(() => {
         setValue(initialValue ?? '')
-    }, [initialValue, setValue
+    }, [initialValue, setValue])
             
     const ref = useOnclickOutside(() => {
         // When user clicks outside of the component, we can dismiss
