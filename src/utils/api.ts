118faebf203643ed4ab260aeea31584259c19a06
jaredgoldman <jaredgoldmandev@gmail.com>
2024-02-06 16:14:54 -0500

    fix: use BASE_URL var for router

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,15 +4,16 @@
  *
  * We also create a few inference helpers for input and output types.
  */
-import { httpBatchLink, loggerLink } from "@trpc/client"
-import { createTRPCNext } from "@trpc/next"
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server"
-import superjson from "superjson"
-import { type AppRouter } from "~/server/api/root"
+import { httpBatchLink, loggerLink } from '@trpc/client'
+import { createTRPCNext } from '@trpc/next'
+import { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
+import superjson from 'superjson'
+import { AppRouter } from '~/server/api/root'
+import { env } from '~/env.mjs'
 
 const getBaseUrl = () => {
-    if (typeof window !== "undefined") return "" // browser should use relative url
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
+    if (typeof window !== 'undefined') return '' // browser should use relative url
+    if (env.BASE_URL) return `https://${env.BASE_URL}` // SSR should use vercel url
     return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
 }
 
@@ -35,14 +36,14 @@ export const api = createTRPCNext<AppRouter>({
             links: [
                 loggerLink({
                     enabled: (opts) =>
-                        process.env.NODE_ENV === "development" ||
-                        (opts.direction === "down" &&
-                            opts.result instanceof Error),
+                        process.env.NODE_ENV === 'development' ||
+                        (opts.direction === 'down' &&
+                            opts.result instanceof Error)
                 }),
                 httpBatchLink({
-                    url: `${getBaseUrl()}/api/trpc`,
-                }),
-            ],
+                    url: `${getBaseUrl()}/api/trpc`
+                })
+            ]
         }
     },
     /**
@@ -50,7 +51,7 @@ export const api = createTRPCNext<AppRouter>({
      *
      * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
      */
-    ssr: false,
+    ssr: false
 })
 
 /**
