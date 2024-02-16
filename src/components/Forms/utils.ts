579e88dbf4b7b78bd4d7c52ae6e25aec64b98e39
jaredgoldman <jaredgoldmandev@gmail.com>
2024-02-16 17:05:32 -0500

    feat: make dark mode mandatory

--- src/components/Forms/utils.ts
+++ src/components/Forms/utils.ts
@@ -1,3 +1,4 @@
+// Ensure we have a valid file name by trimming any whitespace
 export const trimFileName = (originalFile: File) => {
     const trimmedName = originalFile.name.trim()
     return new File([originalFile], trimmedName, {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import { Config } from 'tailwindcss'
 
 export default {
-    darkMode: 'class',
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
     mode: 'jit',
     purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
