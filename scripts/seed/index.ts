c4f5cb6b3f84097fbd84eddc0ad485ff748cd851
jaredgoldman <jaredgoldmandev@gmail.com>
2024-07-13 18:46:23 -0400

    fix: remove scripts file

--- scripts/seed/index.ts
+++
@@ -1,8 +0,0 @@
-import { generateAdmin } from './utils'
-import { prisma } from '~/server/db'
-import venus from './data/venues.json'
-
-export const seed = async () => {
-    await generateAdmin()
-    await prisma.venue.createMany({ data: venus })
-}
