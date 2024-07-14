5852aa071bef4262b3a68a0959eb1e5cde9e0b98
jaredgoldman <jaredgoldmandev@gmail.com>
2024-07-14 19:19:56 -0400

    feat: lint

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 /**
  * Hook to use local storage
@@ -9,7 +9,7 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
     const [storedValue, setStoredValue] = useState<T>(() => {
         try {
             const item = window.localStorage.getItem(key)
-            return item ? JSON.parse(item) : initialValue
+            return item ? JSON.parse(item) as T : initialValue
         } catch (error) {
             console.error(error)
             return initialValue
@@ -18,7 +18,8 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
 
     const setValue = (value: T | ((val: T) => T)) => {
         try {
-            const valueToStore = value instanceof Function ? value(storedValue) : value
+            const valueToStore =
+                value instanceof Function ? value(storedValue) : value
             setStoredValue(valueToStore)
             window.localStorage.setItem(key, JSON.stringify(valueToStore))
         } catch (error) {
