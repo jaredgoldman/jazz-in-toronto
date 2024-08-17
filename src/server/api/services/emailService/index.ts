7ec95ba826282a3a6a79f1ff5d14c548c1ff4f79
jaredgoldman <jaredgoldmandev@gmail.com>
2024-08-17 15:32:18 -0400

    feat: add pending approval and approval email methods to emailService

--- src/server/api/services/emailService/index.ts
+++ src/server/api/services/emailService/index.ts
@@ -1,8 +1,17 @@
 import nodemailer from 'nodemailer'
-import { env } from 'process'
+import { env } from '~/env.mjs'
+import { Artist, Event, Venue } from '@prisma/client'
 
+/**
+ * A service for sending emails
+ * @class
+ * @classdesc A service for sending emails
+ * @exports EmailService
+ * @requires nodemailer
+ */
 export default class EmailService {
     private transport
+    private adminEmail = env.EMAIL_SERVER_USER
     constructor() {
         this.transport = nodemailer.createTransport({
             host: env.EMAIL_SERVER_HOST,
@@ -14,7 +23,15 @@ export default class EmailService {
         })
     }
 
-    async sendEmail(
+    /**
+     * Send an email
+     * @param {string} from The email address to send from
+     * @param {string} to The email address to send to
+     * @param {string} subject The email subject
+     * @param {string} text The email body as plain text
+     * @param {string} html The email body as HTML
+     */
+    public async sendEmail(
         from: string,
         to: string,
         subject?: string,
@@ -28,5 +45,55 @@ export default class EmailService {
             text,
             html
         })
+    }
+
+    /**
+     * Send an email indicating that an item is pending approval
+     * @param {string} to The email address to send to
+     * @param {'Artist' | 'Event' | 'Venue'} type The type of item
+     * @param {Artist | Event | Venue} item The item to approve
+     */
+    public async sendPendingApprovalEmail(
+        to: string,
+        type: 'Artist' | 'Event' | 'Venue',
+        item: Artist | Event | Venue
+    ) {
+        await this.sendEmail(
+            this.adminEmail,
+            to,
+            `Jazz In Toronto: ${type} Pending Approval`,
+            `Your request to add ${type.toLowerCase()}: "${
+                item.name
+            }" is pending approval. We'll send you an email once it's approved.
+
+Best Regards,
+Jazz In Toronto team
+https://jazzintoronto.ca`
+        )
+    }
+
+    /**
+     * Send an email indicating that an item has been approved
+     * @param {string} to The email address to send to
+     * @param {'Artist' | 'Event' | 'Venue'} type The type of item
+     * @param {Artist | Event | Venue} item The item to approve
+     */
+    public async sendApprovedEmail(
+        to: string,
+        type: 'Artist' | 'Event' | 'Venue',
+        item: Artist | Event | Venue
+    ) {
+        await this.sendEmail(
+            this.adminEmail,
+            to,
+            `Jazz In Toronto: ${type} Approved!`,
+            `Your request to add ${type.toLowerCase()}: "${
+                item.name
+            }" has been approved and now live on the site!
+
+Best Regards,
+Jazz In Toronto team
+https://jazzintoronto.ca`
+        )
     }
 }
